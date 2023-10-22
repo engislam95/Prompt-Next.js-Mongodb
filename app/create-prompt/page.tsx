bf933c600ac22ef860255743fdbd7fcc860d2ccb
engislam95 <islam.baidaq@gmail.com>
2023-10-22 14:46:49 +0200

    fix deploy conflict wrapping in if statement

--- app/create-prompt/page.tsx
+++ app/create-prompt/page.tsx
@@ -18,18 +18,21 @@ const CreatePrompt = () => {
     setIsSubmitting(true);
 
     try {
-      const response = await fetch("/api/prompt/new", {
-        method: "POST",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          userId: session.user.id,
-          tag: post.tag,
-        }),
-      });
-
-      if (response.ok) {
-        router.push("/");
+      if(session) {
+        const response = await fetch("/api/prompt/new", {
+          method: "POST",
+          body: JSON.stringify({
+            prompt: post.prompt,
+            userId: session?.user.id,
+            tag: post.tag,
+          }),
+        });
+        if (response.ok) {
+          router.push("/");
+        }
       }
+     
+     
     } catch (error) {
       console.log(error);
     } finally {
